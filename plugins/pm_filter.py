a5730e5ef0f2aa81d806a12a35db2ce785e103f2
Jkdeveloper01 <142569922+Jkdeveloper01@users.noreply.github.com>
2023-08-25 15:44:51 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1571,7 +1571,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             parse_mode=enums.ParseMode.HTML
         )
     elif query.data.startswith("generate_stream_link"):
-        _, file_id = data.split(":")
+        _, file_id = query.data.split(":")
         try:
             user_id = query.from_user.id
             username =  query.from_user.mention 
