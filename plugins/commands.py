303c76e66cd23a15a11ac26991d746bee0b7612b
Jkdeveloper01 <142569922+Jkdeveloper01@users.noreply.github.com>
2023-09-24 07:45:23 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -277,7 +277,7 @@ async def start(client, message):
             logger.exception(e)
             f_caption=f_caption
     if f_caption is None:
-        f_caption = f"{files.file_name}"
+        f_caption = f"{' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), files.file_name.split()))}"
     if not await check_verification(client, message.from_user.id) and VERIFY == True:
         btn = [[
             InlineKeyboardButton("Verify", url=await get_token(client, message.from_user.id, f"https://t.me/{temp.U_NAME}?start="))
