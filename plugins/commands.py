801e568a79e244790a8c2f29ec3fa159833407f0
Jkdeveloper01 <142569922+Jkdeveloper01@users.noreply.github.com>
2023-10-17 14:59:28 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,10 +1,7 @@
 import os
-import logging, pyrogram
+import logging,
 import random
 import asyncio
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-from os import environ
 from Script import script
 from pyrogram import Client, filters, enums
 from pyrogram.errors import ChatAdminRequired, FloodWait
@@ -19,58 +16,9 @@ import re
 import json
 import base64
 logger = logging.getLogger(__name__)
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-usercaption_position = CAPTION_POSITION
-caption_position = usercaption_position.lower()
-CAPTION_TEXT = os.environ.get("CAPTION_TEXT", "")
-CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "bottom")
 
 BATCH_FILES = {}
 
-@Client.on_message(filters.command("cstart") & filters.incoming)
-async def start(bot, message):
-        await message.reply("<I>I Am Auto Caption Bot Just Add me as a Admin in your channel with edit permission and See Magic</I>\n\n• **support**")
-
-@Client.on_message(filters.channel & (filters.document | filters.video | filters.audio ) & ~filters.edited, group=-1)
-async def editing(bot, message):
-      try:
-         media = message.document or message.video or message.audio
-         caption_text = CAPTION_TEXT
-      except:
-         caption_text = ""
-         pass 
-      if (message.document or message.video or message.audio): 
-          if message.caption:                        
-             file_caption = f"**{message.caption}**"                
-          else:
-             fname = media.file_name
-             filename = fname.replace("_", ".")
-             file_caption = f"`{filename}`"  
-              
-      try:
-          if caption_position == "top":
-             await bot.edit_message_caption(
-                 chat_id = message.chat.id, 
-                 message_id = message.message_id,
-                 caption = caption_text + "\n" + file_caption,
-                 parse_mode = "markdown"
-             )
-          elif caption_position == "bottom":
-             await bot.edit_message_caption(
-                 chat_id = message.chat.id, 
-                 message_id = message.message_id,
-                 caption = file_caption + "\n\n" + caption_text,
-                 parse_mode = "markdown"
-             )
-          elif caption_position == "nil":
-             await bot.edit_message_caption(
-                 chat_id = message.chat.id,
-                 message_id = message.message_id,
-                 caption = caption_text, 
-                 parse_mode = "markdown"
-             ) 
-      except:
-          pass
 
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
