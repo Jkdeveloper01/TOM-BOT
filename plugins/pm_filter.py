272842fd9e3624a3d549c0c0ff691df4d37582ad
Jkdeveloper01 <142569922+Jkdeveloper01@users.noreply.github.com>
2023-11-17 19:45:07 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -505,7 +505,7 @@ async def filter_languages_cb_handler(client: Client, query: CallbackQuery):
     offset = 0
     off_set = 0
     n_offset == 0
-    off_set = None:
+    off_set = None
 
     btn.append([InlineKeyboardButton("⇚ ʙᴀᴄᴋ​", callback_data=f"next_{req}_{key}_{off_set}"), InlineKeyboardButton(f"{math.ceil(int(offset)/int(MAX_B_TN))+1} / {math.ceil(total/int(MAX_B_TN))}", callback_data="pages")])
     offset is None:
